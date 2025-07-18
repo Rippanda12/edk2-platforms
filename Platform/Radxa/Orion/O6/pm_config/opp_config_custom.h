cd0b043655bec6ab624f5721308ca6a31e9cb57f
rippanda12 <panda@bredos.org>
2025-07-18 11:09:43 +0300

    2.6ghz is stable

--- Platform/Radxa/Orion/O6/pm_config/opp_config_custom.h
+++ Platform/Radxa/Orion/O6/pm_config/opp_config_custom.h
@@ -48,8 +48,8 @@ static domain_opp_config_t dxs_lit = {
 };
 
 static domain_opp_config_t dxs_gb0 = {
-    .size = 8,
-    .sustained_idx = 7,
+    .size = 7,
+    .sustained_idx = 6,
     .opp_table = {
         { .level =  800UL, .voltage = 750 },
         { .level = 1200UL, .voltage = 750 },
@@ -57,14 +57,13 @@ static domain_opp_config_t dxs_gb0 = {
         { .level = 1800UL, .voltage = 790 },
         { .level = 2200UL, .voltage = 790 },
         { .level = 2400UL, .voltage = 850 },
-        { .level = 2600UL, .voltage = 920 },
-        { .level = 2700UL, .voltage = 940 },   /* sustained */
+        { .level = 2600UL, .voltage = 920 },   /* sustained */
     },
 };
 
 static domain_opp_config_t dxs_gb1 = {
-    .size = 8,
-    .sustained_idx = 7,
+    .size = 7,
+    .sustained_idx = 6,
     .opp_table = {
         { .level =  800UL, .voltage = 750 },
         { .level = 1200UL, .voltage = 750 },
@@ -72,8 +71,7 @@ static domain_opp_config_t dxs_gb1 = {
         { .level = 1800UL, .voltage = 790 },
         { .level = 2200UL, .voltage = 790 },
         { .level = 2400UL, .voltage = 850 },
-        { .level = 2600UL, .voltage = 920 },
-        { .level = 2700UL, .voltage = 940 },   /* sustained */
+        { .level = 2600UL, .voltage = 920 },   /* sustained */
     },
 };
 
